964194c9351421d3cd8023dc5a3c237230385df5
Ryan <ryanshaunsmith@gmail.com>
2025-01-02 17:24:48 +0000

    add test for a shuffled deck

--- tests/deck.test.js
+++ tests/deck.test.js
@@ -1,11 +1,12 @@
-import { createDeck } from "../src/script.js";
+import { createDeck, shuffleDeck } from '../src/script.js';
 
-describe("A valid new deck of cards is created", () => {
-  test("The deck contains 52 cards", () => {
+describe('A valid new deck of cards is created and shuffled', () => {
+  test('The deck contains 52 cards', () => {
     const deck = createDeck();
     expect(deck.length).toStrictEqual(52);
   });
-  test("The deck contains 13 of each suit", () => {
+
+  test('The deck contains 13 of each suit', () => {
     const deck = createDeck();
     const suitCount = deck.reduce((acc, card) => {
       acc[card.suit] = (acc[card.suit] || 0) + 1;
@@ -13,9 +14,30 @@ describe("A valid new deck of cards is created", () => {
     }, {});
     expect(Object.values(suitCount)).toStrictEqual([13, 13, 13, 13]);
   });
-  test("The deck contains no duplicate cards", () => {
+
+  test('The deck contains no duplicate cards', () => {
     const deck = createDeck();
     const cardSet = new Set(deck.map((card) => `${card.value}-${card.suit}`));
     expect(cardSet.size).toStrictEqual(deck.length);
+  });
+
+  test('The deck is shuffled', () => {
+    const originalDeck = createDeck();
+    const shuffledDeck = shuffleDeck([...originalDeck]);
+
+    // Check that the shuffled deck is not in the same order as the original deck
+    const isSameOrder = originalDeck.every(
+      (card, index) => card === shuffledDeck[index]
+    );
+    expect(isSameOrder).toBe(false);
+
+    // Check that the shuffled deck contains the same cards as the original deck
+    const originalDeckSet = new Set(
+      originalDeck.map((card) => `${card.value}-${card.suit}`)
+    );
+    const shuffledDeckSet = new Set(
+      shuffledDeck.map((card) => `${card.value}-${card.suit}`)
+    );
+    expect(originalDeckSet).toStrictEqual(shuffledDeckSet);
   });
 });
