745b9b6c9f24947abcdcff915a109598d9908298
Ryan <ryanshaunsmith@gmail.com>
2025-01-03 16:04:36 +0000

    write tests for player creation

--- tests/deck.test.js
+++ tests/deck.test.js
@@ -1,4 +1,4 @@
-import { createDeck, shuffleDeck } from '../src/script.js';
+import { createDeck, shuffleDeck, createPlayers } from '../src/script.js';
 
 describe('A valid new deck of cards is created and shuffled', () => {
   test('The deck contains 52 cards', () => {
@@ -39,5 +39,21 @@ describe('A valid new deck of cards is created and shuffled', () => {
       shuffledDeck.map((card) => `${card.value}-${card.suit}`)
     );
     expect(originalDeckSet).toStrictEqual(shuffledDeckSet);
+  });
+});
+
+describe('Players are created to play the game', () => {
+  test('Player 1 is created with a name, id, score and a hand', () => {
+    const players = createPlayers(1);
+    expect(players).toBe([{ Name: 'Player 1', id: 1, score: 0, hand: [] }]);
+  });
+  test('Player 2 exists if 2 players are created', () => {
+    const players = createPlayers(2);
+    const player2 = players[1];
+    expect(player2).toBe({ Name: 'Player 2', id: 2, score: 0, hand: [] });
+  });
+  test('The players array contains 4 objects if 4 players selected', () => {
+    const players = createPlayers(4);
+    expect(players.length).toStrictEqual(4);
   });
 });
