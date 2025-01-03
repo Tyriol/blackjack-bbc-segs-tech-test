c2cddb556424714758f6ef4fa9b0fa23354597a3
Ryan <ryanshaunsmith@gmail.com>
2025-01-03 16:30:12 +0000

    fix player creation tests

--- tests/deck.test.js
+++ tests/deck.test.js
@@ -43,17 +43,25 @@ describe('A valid new deck of cards is created and shuffled', () => {
 });
 
 describe('Players are created to play the game', () => {
-  test('Player 1 is created with a name, id, score and a hand', () => {
-    const players = createPlayers(1);
-    expect(players).toBe([{ Name: 'Player 1', id: 1, score: 0, hand: [] }]);
+  test('Players are initialised with a Name and id', () => {
+    const numPlayers = 3;
+    const players = createPlayers(numPlayers);
+    for (let i = 0; i < numPlayers; i++) {
+      expect(players[i].name).toBe(`Player ${i + 1}`);
+      expect(players[i].id).toBe(i + 1);
+    }
   });
-  test('Player 2 exists if 2 players are created', () => {
-    const players = createPlayers(2);
-    const player2 = players[1];
-    expect(player2).toBe({ Name: 'Player 2', id: 2, score: 0, hand: [] });
+  test('Players are initialised with a zero score and empty hand', () => {
+    const numPlayers = 3;
+    const players = createPlayers(numPlayers);
+    for (let i = 0; i < numPlayers; i++) {
+      expect(players[i].score).toBe(0);
+      expect(players[i].hand).toEqual([]);
+    }
   });
-  test('The players array contains 4 objects if 4 players selected', () => {
-    const players = createPlayers(4);
-    expect(players.length).toStrictEqual(4);
+  test('The players array is created with the correct number of players', () => {
+    const numPlayers = 2;
+    const players = createPlayers(numPlayers);
+    expect(players.length).toEqual(numPlayers);
   });
 });
