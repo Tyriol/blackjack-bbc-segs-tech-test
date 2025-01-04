c94bfa2c29fba28feaeaefe0eec8c056482a22cb
Ryan <ryanshaunsmith@gmail.com>
2025-01-04 14:46:56 +0000

    write test for dealing cards

--- tests/deck.test.js
+++ tests/deck.test.js
@@ -1,4 +1,9 @@
-import { createDeck, shuffleDeck, createPlayers } from '../src/script.js';
+import {
+  createDeck,
+  shuffleDeck,
+  createPlayers,
+  dealCards,
+} from '../src/script.js';
 
 describe('A valid new deck of cards is created and shuffled', () => {
   test('The deck contains 52 cards', () => {
@@ -22,18 +27,18 @@ describe('A valid new deck of cards is created and shuffled', () => {
   });
 
   test('The deck is shuffled', () => {
-    const originalDeck = createDeck();
-    const shuffledDeck = shuffleDeck([...originalDeck]);
+    const deck = createDeck();
+    const shuffledDeck = shuffleDeck([...deck]);
 
     // Check that the shuffled deck is not in the same order as the original deck
-    const isSameOrder = originalDeck.every(
+    const isSameOrder = deck.every(
       (card, index) => card === shuffledDeck[index]
     );
     expect(isSameOrder).toBe(false);
 
     // Check that the shuffled deck contains the same cards as the original deck
     const originalDeckSet = new Set(
-      originalDeck.map((card) => `${card.value}-${card.suit}`)
+      deck.map((card) => `${card.value}-${card.suit}`)
     );
     const shuffledDeckSet = new Set(
       shuffledDeck.map((card) => `${card.value}-${card.suit}`)
@@ -63,5 +68,18 @@ describe('Players are created to play the game', () => {
     const numPlayers = 2;
     const players = createPlayers(numPlayers);
     expect(players.length).toEqual(numPlayers);
+  });
+});
+
+describe('Players are dealt an opening hand', () => {
+  test('Players all have 2 cards after the initial deal', () => {
+    const deck = createDeck();
+    shuffleDeck(deck);
+    const numPlayers = 3;
+    const players = createPlayers(numPlayers);
+    dealCards(deck, players);
+    for (let i = 0; i < numPlayers; i++) {
+      expect(players[i].hand.length).toBe(2);
+    }
   });
 });
