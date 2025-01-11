71f81a4694f9d2d13397fb3de397da5d8232f372
Ryan <ryanshaunsmith@gmail.com>
2025-01-11 17:21:35 +0000

    add most of the game functionality to the react app, looping through players, hitting, staying and restarting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import Player from './components/Player/Player.jsx';
+
 import './App.css';
 
 import {
@@ -11,30 +13,83 @@ import {
 
 function App() {
   // state
-  const [isGameStarted, setIsGameStarted] = useState(false);
   const [deck, setDeck] = useState([]);
+  const [numPlayers, setNumPlayers] = useState(1);
   const [players, setPlayers] = useState([]);
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [gameWinner, setGameWinner] = useState('');
 
   const handleStart = (e) => {
     e.preventDefault();
-    console.log(e);
     const newDeck = createDeck();
     const shuffledDeck = shuffleDeck([...newDeck]);
-    const newPlayers = createPlayers(4);
+    const newPlayers = createPlayers(numPlayers);
     const dealtDeck = dealCards([...shuffledDeck], newPlayers);
+    for (let i = 0; i < newPlayers.length; i++) {
+      calculateScore(newPlayers[i]);
+    }
     setDeck(dealtDeck);
     setPlayers(newPlayers);
     setIsGameStarted(true);
+  };
+
+  const handleHit = () => {
+    const updatedPlayers = [...players];
+    const card = deck.pop();
+    updatedPlayers[currentPlayerIndex].hand.push(card);
+    calculateScore(updatedPlayers[currentPlayerIndex]);
+
+    if (updatedPlayers[currentPlayerIndex].score > 21) {
+      handleNextPlayer();
+    }
+
+    setPlayers(updatedPlayers);
+    setDeck([...deck]);
+  };
+
+  const handleStay = () => {
+    handleNextPlayer();
+  };
+
+  const handleNextPlayer = () => {
+    if (currentPlayerIndex < players.length - 1) {
+      setCurrentPlayerIndex(currentPlayerIndex + 1);
+    } else {
+      setCurrentPlayerIndex(-1);
+      let winner = -1;
+      let winningScore = 0;
+      for (let i = 0; i < players.length; i++) {
+        if (players[i].score > winningScore && players[i].score < 22) {
+          winningScore = players[i].score;
+          winner = i;
+        }
+      }
+      setGameWinner(players[winner].name);
+    }
+  };
+
+  const handlePlayAgain = () => {
+    setGameWinner('');
+    setDeck([]);
+    setNumPlayers(1);
+    setPlayers([]);
+    setCurrentPlayerIndex(0);
+    setIsGameStarted(false);
   };
 
   return (
     <>
       <h1>BlackJack</h1>
       {!isGameStarted ? (
-        <form>
+        <form className="player-select">
           <label>
             Choose how many players
-            <select name="players" id="players">
+            <select
+              name="players"
+              id="players"
+              onChange={(e) => setNumPlayers(e.target.value)}
+            >
               <option value="1">One Player</option>
               <option value="2">Two Players</option>
               <option value="3">Three Players</option>
@@ -44,27 +99,27 @@ function App() {
           <button onClick={handleStart}>Start</button>
         </form>
       ) : (
-        <div className="players">
-          {players.map((player) => {
-            calculateScore(player);
-            return (
-              <div className="player" key={player.id}>
-                <h2>{player.name}</h2>
-                <div className="player-cards">
-                  {player.hand.map((card) => {
-                    return (
-                      <div className="card">
-                        <p>{card.value}</p>
-                        <p>{card.suit}</p>
-                      </div>
-                    );
-                  })}
-                </div>
-                <p>Score: {player.score}</p>
-              </div>
-            );
-          })}
-        </div>
+        <>
+          <div className="players">
+            {players.map((player, index) => {
+              return (
+                <Player
+                  key={player.id}
+                  player={player}
+                  isActive={index === currentPlayerIndex}
+                  onHit={handleHit}
+                  onStay={handleStay}
+                />
+              );
+            })}
+          </div>
+          {gameWinner && (
+            <div className="game-over">
+              <p>{gameWinner} is the winner!</p>
+              <button onClick={handlePlayAgain}>Play Again</button>
+            </div>
+          )}
+        </>
       )}
     </>
   );
