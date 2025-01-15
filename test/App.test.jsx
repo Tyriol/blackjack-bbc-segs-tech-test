823617d2ce57698846c263df650a8abfb3bf2dbf
Ryan <ryanshaunsmith@gmail.com>
2025-01-15 15:44:11 +0000

    update Readme with challenges encountered

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import App from '../../src/App';
+import App from '../src/App';
 
 it('renders title', () => {
   render(<App />);
